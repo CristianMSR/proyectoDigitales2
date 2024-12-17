e25b207a6bfe5d0f29bcfbf641b19631387b10ac
Paula Brizzio <paulabrizzio19@gmail.com>
2024-12-17 18:14:49 -0300

    Creo programa RPI

---
+++ RS232/menuModo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "modoRemoto.h"
+
+//extern void modoLocal();
+extern void modoRemoto();
+
+int main (int argc, char *argv[]) {
+
+  int op, valid;
+
+    printf("Seleccione el modo de trabajo:\n");
+    printf("    1. Modo remoto\n");
+    printf("    2. Modo local\n");
+
+  do{
+
+    printf("\nOpción (1 o 2): ");
+    valid = scanf("%d",&op);
+
+    while(getchar() != '\n');
+
+    if(valid != 1 || op < 1 || op > 2)
+      printf("\n¡ERROR! Opción no válida.\nIngrese 1 o 2.\n");
+
+  }while(valid != 1 || op < 1 || op >2);
+
+  if(op == 1){
+    //modoLocal();
+  }
+  else {
+    modoRemoto();
+  }
+
+  return 0;
+}
