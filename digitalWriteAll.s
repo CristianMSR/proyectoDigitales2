33726b578ee0bf52555c628de92cfad801636355
Paula Brizzio <paulabrizzio19@gmail.com>
2024-12-19 05:45:45 -0300

    Cambio funcion .s

--- digitalWriteAll.s
+++ digitalWriteAll.s
@@ -7,15 +7,17 @@ leds:           .word     26, 21, 20, 16, 12, 25, 24, 23
                
                 //Función para escribir en todos los LEDs basados en un valor binario
 digitalWriteAll:
-                mov     r1, r0             
-                mov     r2, #0             
+                mov     r6, r0            
+                mov     r2, #0            
                
 loop:
-                lsr     r3, r1, r2
-                and     r3, r3, #0x01
+                lsr     r1, r6, r2
+                and     r1, r1, #0x01
                 ldr     r4, =leds          
-                ldr     r5, [r4, r2, lsl #2] 
-                bl      digitalWrite     
+                ldr     r0, [r4, r2, lsl #2]
+               
+                bl      digitalWrite    
+               
                 add     r2, r2, #1
                 cmp     r2, #8
                 bne     loop
